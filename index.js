cd455b6b615c7eba7c0bdca73f8383e0cfa8928d
frankbjj23 <frankbjj23@gmail.com>
2025-10-30 22:00:55 -0400

    assignment completed

---
+++ index.js
@@ -0,0 +1,23 @@
+cons = code1 = 7 + 3;
+
+console.log(code1);
+
+cons = code2 = 100 / 2 - 10;
+
+console.log(code2);
+
+cons = code3 = (3 % 2) + 38;
+
+console.log(code3);
+
+cons = message = "The vault has been secured. The combination is:";
+
+console.log(message);
+
+cons = codeA = code1 + "-" + code2 + "-" + code3;
+
+console.log(codeA);
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(codeB);
